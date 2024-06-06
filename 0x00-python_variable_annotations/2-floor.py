adebc2a6576ec880049de66a77a5a4d49377d3b0
Tau <taumasaire@gmail.com>
2024-06-06 16:54:15 +0200

    adding basic annotation scripts

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module has a type-annotated function floor"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """returns the floor of a float"""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module has a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """Return string representation of a float"""
+    return str(n)
