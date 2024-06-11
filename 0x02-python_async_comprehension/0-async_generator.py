4746146bc8fb776bc6e21b2fea8b3e8357374954
Tau <taumasaire@gmail.com>
2024-06-11 12:15:22 +0200

    adding async comprehension script

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,9 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """This function returns a random number between 0 and 10"""
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+    try:
+        for _ in range(10):
+            await asyncio.sleep(1)
+            yield random.uniform(0, 10)
+    except Exception as e:
+        pass
