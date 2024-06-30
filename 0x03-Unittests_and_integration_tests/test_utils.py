b0acad68d5b5215725d302b64211aab61f5f7fdf
Tau <taumasaire@gmail.com>
2024-06-30 12:52:58 +0200

    adding the parameterize a unit test script

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+                        self, nested_map, path, expected_output):
+        """Test access_nested_map function raises exception"""
+        with self.assertRaises(expected_output):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
